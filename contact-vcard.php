73676e77800adfe42a3e6f7c7be1ac3459b27476
Sander Drenth <sander@sterc.nl>
2015-07-21 16:07:47 +0200

    Added \n at line ending (windows fix)

--- contact-vcard.php
+++ contact-vcard.php
@@ -1,5 +1,5 @@
 <?php
-(@include_once '../../config.core.php') or die("Configuratiebestand is niet gevonden");
+(@include_once '../../config.core.php') or die("core configuration file not found");
 require_once MODX_CORE_PATH.'model/modx/modx.class.php';
 $modx = new modX();
 $modx->initialize('web');
@@ -8,86 +8,74 @@ $path = $modx->getOption('clientconfig.core_path', null, $modx->getOption('core_
 $path .= 'model/clientconfig/';
 $clientConfig = $modx->getService('clientconfig','ClientConfig', $path);
 
-/* If we got the class (gotta be careful of failed migrations), grab settings and go! */
 if ($clientConfig instanceof ClientConfig) {
     $settings = $clientConfig->getSettings();
 }
 
 $companyName 			= $modx->getOption('site_name');
-$street 				= $settings['street'];
-$housenumber 			= $settings['housenumber'];
-$zipcode 				= $settings['zipcode'];
-$city 					= $settings['city'];
-$fax 					= $settings['fax'];
-
 $siteUrl				= $modx->getOption('site_url');
 
-if(isset($_GET['id'])){
-	//get tvs
+if($_GET['id']){
 	$resource 			= $modx->getObject('modResource', $_GET['id']);
-	if($resource){
-		$email 				= $resource->getTVValue('emailTVname');
-		$phone				= $resource->getTVValue('phoneTVname');
-		$image				= $resource->getTVValue('photoTVname');
-		$fullName 			= $resource->get('pagetitle');
-	}
-	else {
-		$modx->log(modX::LOG_LEVEL_ERROR,'Resource with ID: ' . $_GET['id'] . ' was not found.');
-	}
+
+	$image				= $resource->getTVValue('photo');
+	$street				= $resource->getTVValue('street');
+	$housenumber		= $resource->getTVValue('housenumber');
+	$zipcode			= $resource->getTVValue('zipcode');
+	$city				= $resource->getTVValue('city');
+	$fax				= $resource->getTVValue('fax');
+	$phone				= $resource->getTVValue('phone');
+	$email 				= $resource->getTVValue('email');
+	$fullName 			= $resource->get('pagetitle');
+
 }
 else {
-	//get options
-	
+	$image				= $settings['vcard_logo'];
 	$street 			= $settings['street'];
 	$housenumber 		= $settings['housenumber'];
 	$zipcode 			= $settings['zipcode'];
 	$city 				= $settings['city'];
-	$phone 				= $settings['phone'];
 	$fax 				= $settings['fax'];
-	$email 				= $settings['email_client'];
-	$image				= $settings['vcard_logo'];
-
+	$phone 				= $settings['phone'];
+	$email 				= $settings['email_client'];	
 	$fullName 			= $companyName; 
 }
 
-
-$getPhoto	= file_get_contents($siteUrl . $image);
-
-$b64vcard = base64_encode($getPhoto);
-$b64mline = chunk_split($b64vcard,74,"\n");
-$b64final = preg_replace('/(.+)/', ' $1', $b64mline);
-
-$photo = $b64final;
+$getPhoto				= file_get_contents($siteUrl . $image);
+$b64vcard 				= base64_encode($getPhoto);
+$b64mline 				= chunk_split($b64vcard,74,"\n");
+$b64final 				= preg_replace('/(.+)/', ' $1', $b64mline);
+$photo 					= $b64final;
 
 header('Content-Type: text/x-vcard');  
 header('Content-Disposition: inline; filename= "vCard.vcf"');  
 
-$vCard = "BEGIN:VCARD\r";
-$vCard .= "VERSION:3.0\r";
-$vCard .= "FN:" . $fullName . "\r";
-$vCard .= "TITLE:" . $companyName . "\r";
+$vCard = "BEGIN:VCARD\r\n";
+$vCard .= "VERSION:3.0\r\n";
+$vCard .= "FN:" . $fullName . "\r\n";
+$vCard .= "TITLE:" . $companyName . "\r\n";
 
 if($street or $zipcode OR $city){
-	$vCard .= "ADR;TYPE=work:;;" . $street . ' ' . $housenumber . ";" . $city . ";IL;" . $zipcode . ";\r";
+	$vCard .= "ADR;TYPE=work:;;" . $street . ' ' . $housenumber . ";" . $city . ";IL;" . $zipcode . ";\r\n";
 }
 
 if($fax){
-	$vCard .= "TEL;FAX;WORK:" . $fax . "\r";
+	$vCard .= "TEL;FAX;WORK:" . $fax . "\r\n";
 }
 
 if($email){
-	$vCard .= "EMAIL;TYPE=internet,pref:" . $email . "\r";
+	$vCard .= "EMAIL;TYPE=internet,pref:" . $email . "\r\n";
 }
 
 if($getPhoto){
-	$vCard .= "PHOTO;ENCODING=b;TYPE=JPEG:\r";
-	$vCard .= $photo;
+	$vCard .= "PHOTO;ENCODING=b;TYPE=JPEG:";
+	$vCard .= $photo . "\r\n";
 }
 
 if($phone){
-	$vCard .= "TEL;TYPE=work,voice:" . $phone . "\r";	
+	$vCard .= "TEL;TYPE=work,voice:" . $phone . "\r\n";	
 }
 
-$vCard .= "END:VCARD";
+$vCard .= "END:VCARD\r\n";
 
 echo $vCard;
